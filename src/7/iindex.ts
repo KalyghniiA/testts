218ce86a5bd602b97864cd44723396e1d731e306
Александр <kalyghnii@gmail.com>
2025-10-16 16:04:34 +0500

    first push

--- src/7/iindex.ts
+++ src/7/iindex.ts
@@ -1,9 +1,9 @@
 type Prev = Record<string, number>;
 type Next = Record<number, string>;
 
-const swapKeysAndValues = <T extends Prev> (prevObj : T): Next => {
+export const swapKeysAndValues = <T extends Prev> (prevObj : T): Next => {
     const keys = Object.keys(prevObj);
-    if (keys.length === 0) throw new Error(`keys must be ${keys.length}`);
+
     return keys
         .map(key => {
             const newKey = prevObj[key];
